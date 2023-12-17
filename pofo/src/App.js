c6b7d67e5278ac7ee2249a4f903c13d22a48676f
Alex Le <alexkaile@gmail.com>
2023-12-16 20:14:57 -0800

    icons

--- pofo/src/App.js
+++ pofo/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import Navbar from './Navbar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fab);
+library.add(faEnvelope);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   {/* <header className="App-header">
+    //   </header> */}
+    //   <Navbar/>
+    // </div>
+    <Navbar/>
   );
 }
 
---
+++ pofo/src/Navbar.js
@@ -0,0 +1,74 @@
+// VerticalNavbar.js
+import React, { useState } from 'react';
+import './Navbar.css';
+import profileImg from './images/profile_grad.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+
+const VerticalNavbar = () => {
+    const [isHovered, setIsHovered] = useState(false);
+    const linkedinIconStyle = { color: '#ffffff' }; // Default color
+    const linkedinIconHoverStyle = { color: '#0077B5' }; // Hover color
+
+
+    return (
+        <div className="vertical-navbar">
+        <div className="profile-container">
+            {/* <div className="profile-image"></div> */}
+            <img className="profile-image" src={profileImg} alt="Contact 1" />
+            <div className="contact-buttons">
+            <button className="contact-button">
+                <FontAwesomeIcon
+                icon={['fab', 'linkedin']}
+                style={isHovered ? linkedinIconHoverStyle : linkedinIconStyle}
+                fontSize='24px'
+                className="linkedin-icon"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}/>
+            </button>
+            <button className="contact-button">
+                <FontAwesomeIcon
+                icon={['fab', 'github']}
+                style={isHovered ? linkedinIconHoverStyle : linkedinIconStyle}
+                fontSize='24px'
+                className="linkedin-icon"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}/>
+            </button>
+            <button className="contact-button">
+                <FontAwesomeIcon
+                icon="envelope"
+                style={isHovered ? linkedinIconHoverStyle : linkedinIconStyle}
+                fontSize='24px'
+                className="linkedin-icon"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}/>
+            </button>
+            <button className="contact-button">
+                <FontAwesomeIcon
+                icon={faFile}
+                style={isHovered ? linkedinIconHoverStyle : linkedinIconStyle}
+                fontSize='24px'
+                className="linkedin-icon"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}/>
+            </button>
+            <FontAwesomeIcon icon="fa-regular fa-file" />
+            </div>
+        </div>
+        <FontAwesomeIcon icon="fa-regular fa-file" />
+        <div className="description-box">
+            <p>Computer Science</p>
+            <p>Molecular Biology</p>
+            <p>Industrial Design</p>
+        </div>
+        <button className="nav-button">Welcome</button>
+        <button className="nav-button">Resume</button>
+        <button className="nav-button">Projects</button>
+        <button className="nav-button">Hobbies</button>
+        <button className="nav-button">Contact</button>
+        </div>
+    );
+};
+
+export default VerticalNavbar;
